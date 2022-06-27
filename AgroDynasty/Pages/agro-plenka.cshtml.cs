5efb8d0a31fbe380285a3ae3f25d043819ffff61
Руслан Ключенко <klyuchenkorus@gmail.com>
2022-06-27 13:56:27 +0300

    completed agro-plenka page

---
+++ AgroDynasty/Pages/agro-plenka.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgroDynasty.Pages
+{
+    public class agro_plenkaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

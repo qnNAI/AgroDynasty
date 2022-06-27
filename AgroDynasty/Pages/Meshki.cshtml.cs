68fbc0d6d5c21d3f9a18ef016899a2bd4780dc19
Руслан Ключенко <klyuchenkorus@gmail.com>
2022-06-27 14:52:06 +0300

    added meshki page

---
+++ AgroDynasty/Pages/Meshki.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgroDynasty.Pages
+{
+    public class MeshkiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

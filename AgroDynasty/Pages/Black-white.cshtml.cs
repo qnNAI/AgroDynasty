8162086b1bd9505555aed213211a384cb68609de
Руслан Ключенко <klyuchenkorus@gmail.com>
2022-06-27 14:36:22 +0300

    added black-white page

---
+++ AgroDynasty/Pages/Black-white.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgroDynasty.Pages
+{
+    public class Black_whiteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

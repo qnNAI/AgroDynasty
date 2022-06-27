8b8efba64ba1eb8d31d6396825c948594b48c440
Руслан Ключенко <klyuchenkorus@gmail.com>
2022-06-27 14:11:31 +0300

    added setka-seno page

---
+++ AgroDynasty/Pages/Setka-seno.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgroDynasty.Pages
+{
+    public class Setka_senoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

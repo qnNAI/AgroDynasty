fb4c472bc335deee3437cf62475a831ea095b2d8
Руслан Ключенко <klyuchenkorus@gmail.com>
2022-06-27 14:26:29 +0300

    added shpagat-seno page

---
+++ AgroDynasty/Pages/Shpagat-seno.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgroDynasty.Pages
+{
+    public class Shpagat_senoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
